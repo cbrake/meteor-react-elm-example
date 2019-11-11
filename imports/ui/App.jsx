fdca29c0451a2abd5224726cb3bacd8103670744
Cliff Brake <cbrake@bec-systems.com>
2019-11-11 15:46:08 -0500

    show how to use elm stuff in multiple modules

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import Hello from "./Hello.jsx";
 import Info from "./Info.jsx";
-import ElmStuff from "./ElmStuff.jsx";
+import Chart from "./Chart.jsx";
+import REC from "react-elm-components";
+
+// The below imports the Elm runtime as a global.
+// You can then use any of the Elm modules.
+import ElmMain from "./elm/ElmMain.js";
 
 const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
     <Hello />
-    <ElmStuff />
+    <h3>Start Elm Stuff</h3>
+    <Chart />
+    <REC src={Elm.Button} />
+    <h3>End Elm Stuff</h3>
     <Info />
   </div>
 );
--- imports/ui/Chart.jsx
+++ imports/ui/Chart.jsx
@@ -8,10 +8,7 @@ export default class Graph extends React.Component {
     return (
       <div className="time-container">
         <div>
-          <h3>Start Elm Stuff</h3>
           <REC src={Elm.MyChart} />
-          <REC src={Elm.Button} />
-          <h3>End Elm Stuff</h3>
         </div>
       </div>
     );
